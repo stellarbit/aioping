3838a13992bf0217de5c5f5a0a778ab33e24ad58
Anton Belousov <anton@belousov.co>
2017-01-17 11:18:24 +0300

    Package changes.

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 from distutils.core import setup
 
 setup(
-    name = "aioping",
-    packages = ["aioping"],
-    version = "1.0",
-    description = "Asyncio ping implementation",
-    author = "Anton Belousov",
-    author_email = "anton@stellarbit.com",
-    url = "https://github.com/stellarbit/aioping",
-    download_url = "https://github.com/stellarbit/aioping/tarball/1.0",
-    keywords = ["network", "icmp", "ping", "asyncio"],
-    classifiers = [
+    name="aioping",
+    packages=["aioping"],
+    version="0.1.0",
+    description="Asyncio ping implementation",
+    author="Anton Belousov",
+    author_email="anton@stellarbit.com",
+    url="https://github.com/stellarbit/aioping",
+    download_url="https://github.com/stellarbit/aioping/tarball/1.0",
+    keywords=["network", "icmp", "ping", "asyncio"],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
+    ]
 )
