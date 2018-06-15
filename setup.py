8b2dadcf854d60a305cf13ea62b1ab478868416b
Anton Belousov <anton@belousov.co>
2018-06-15 19:18:34 +0300

    setup.py version updated.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
     name="aioping",
     packages=["aioping"],
-    version="0.1.0",
+    version="0.2.0",
     install_requires=["async_timeout", "aiodns"],
     description="Asyncio ping implementation",
     author="Anton Belousov",
     author_email="anton@stellarbit.com",
     url="https://github.com/stellarbit/aioping",
-    download_url="https://github.com/stellarbit/aioping/tarball/0.1.0",
+    download_url="https://github.com/stellarbit/aioping/tarball/0.2.0",
     keywords=["network", "icmp", "ping", "asyncio"],
     classifiers=[
         "Development Status :: 4 - Beta",
